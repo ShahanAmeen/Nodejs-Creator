918a304e4c62eaacd8695068e7161ec4b4af0083
Shahan Ameen <Shahanameen@gmail.com>
2023-10-23 16:41:27 -0500

    added inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown")
+const inquirer = require("inquirer")
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+[
+    {name:"title",
+    type:"input",
+    message:"What is the title of the app?",
+    },
+    {name:"description",
+    type:"input",
+    message:"Give a description of the application",
+    },
+    /// ad rest of questions here, refer to assignment README
+];  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        console.log(data)
+    })
+
+}
 
 // Function call to initialize app
 init();
